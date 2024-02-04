db28b2eb26da5d4bf54e92af9184fdf5d9104103
Martin Todorov <martin.todorov@kodinsoft.net>
2024-02-04 21:06:21 +0200

    Updated example images

--- watershed.py
+++ watershed.py
@@ -9,7 +9,7 @@ def save_image(image, output_path):
 def apply_threshold(image):
     thresh = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                    cv2.THRESH_BINARY, 11, 2)
-    save_image(thresh, '/images/thresh.png')
+    save_image(thresh, 'images/thresh.png')
     return thresh
 
 def detect_edges_and_draw_contours(threshold_image, grayscale_image):
@@ -24,8 +24,8 @@ def detect_edges_and_draw_contours(threshold_image, grayscale_image):
     result = np.zeros_like(threshold_image)
     # Apply the mask to copy the original color where the mask is true
     result[mask_bool] = threshold_image[mask_bool]
-    cv2.drawContours(grayscale_image, contours, -1, (0, 255, 0), 3)
-    save_image(grayscale_image, 'images/contured_image.png')
+
+    save_image(result, 'images/contured_image.png')
     return result
 
 def apply_watershed(noise_free_img, grayscale_image, original_image):
@@ -64,19 +64,30 @@ def apply_watershed(noise_free_img, grayscale_image, original_image):
         original_image[markers == marker] = colors[i]
 
     save_image(original_image, 'images/watershed_output.png')
+    return markers
+
+def count_stars(markers):
+    unique_markers = np.unique(markers)
+    unique_markers = np.delete(unique_markers, np.where(unique_markers <= 1))
+    return len(unique_markers)
 
 def main(image_path):
+    print("Starting process...")
     image = cv2.imread(image_path)
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    print("Applying threshold...")
     threshold = apply_threshold(gray_img)
+    print("Detecting edges and drawing contours...")
     edges_detected = detect_edges_and_draw_contours(threshold, gray_img)
 
     kernel = np.ones((3,3),np.uint8)
+    print("Applying morphological transformations...")
     opening_image = cv2.morphologyEx(edges_detected, cv2.MORPH_OPEN, kernel)
     save_image(opening_image, 'images/opening.png')
-
-    apply_watershed(opening_image, gray_img, image)
-
+    print("Applying watershed algorithm...")
+    markers = apply_watershed(opening_image, gray_img, image)
+    num_stars = count_stars(markers)
+    print(f"Number of stars found: {num_stars}")
 
 if __name__ == '__main__':
     # Create the parser
