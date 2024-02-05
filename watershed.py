6c3e85f8590e95b410a69cd2a96e605e62d0cde2
Martin Todorov <martin.todorov@kodinsoft.net>
2024-02-05 16:03:46 +0200

    Improvment in algorithm

--- watershed.py
+++ watershed.py
@@ -2,6 +2,8 @@ import argparse
 
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 def save_image(image, output_path):
     cv2.imwrite(output_path, image)
@@ -12,24 +14,27 @@ def apply_threshold(image):
     save_image(thresh, 'images/thresh.png')
     return thresh
 
-def detect_edges_and_draw_contours(threshold_image, grayscale_image):
-    edges = cv2.Canny(threshold_image, 100, 200)
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    mask = np.zeros_like(grayscale_image)  # Create a mask the same size as the gray image
-    for cnt in contours:
-        cv2.drawContours(mask, [cnt], -1, 255, -1)  # Draw filled contour in white
-    # Convert mask to boolean
-    mask_bool = mask.astype(bool)
-    # Create an empty array with the same shape as the original image but filled with black
-    result = np.zeros_like(threshold_image)
-    # Apply the mask to copy the original color where the mask is true
-    result[mask_bool] = threshold_image[mask_bool]
+def detect_connected_components(threshold_image):
+    num_labels, labels_im = cv2.connectedComponents(threshold_image)
+    # Map component labels to hue val, 0-179 is the hue range in OpenCV
+    label_hue = np.uint8(179 * labels_im / np.max(labels_im))
+    blank_ch = 255 * np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
 
-    save_image(result, 'images/contured_image.png')
-    return result
+    # Converting cvt to BGR
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_BGR2GRAY)
+
+    # Set background label to black
+    labeled_img[label_hue == 0] = 0
+    # Set the image to white where the labels are not 0 (not the background)
+    labeled_img[label_hue > 0] = 255
+
+    plt.imsave('images/labaled_image.png', labeled_img)
+    return labeled_img
 
 def apply_watershed(noise_free_img, grayscale_image, original_image):
     kernel = np.ones((3, 3), np.uint8)
+
     # sure background area
     sure_bg = cv2.dilate(noise_free_img, kernel, iterations=1)
     save_image(sure_bg, 'images/sure_bg.png')
@@ -66,6 +71,24 @@ def apply_watershed(noise_free_img, grayscale_image, original_image):
     save_image(original_image, 'images/watershed_output.png')
     return markers
 
+def display_images(image_path):
+    # Load the images
+    image1 = mpimg.imread(image_path)
+    image2 = mpimg.imread('images/watershed_output.png')
+
+    # Create a figure to display the images
+    fig, ax = plt.subplots(1, 2)  # 1 row, 2 columns
+
+    # Display the first image
+    ax[0].imshow(image1)
+    ax[0].axis('off')  # Turn off axis numbering and ticks for the first image
+
+    # Display the second image
+    ax[1].imshow(image2)
+    ax[1].axis('off')  # Turn off axis numbering and ticks for the second image
+
+    plt.show()
+
 def count_stars(markers):
     unique_markers = np.unique(markers)
     unique_markers = np.delete(unique_markers, np.where(unique_markers <= 1))
@@ -75,17 +98,21 @@ def main(image_path):
     print("Starting process...")
     image = cv2.imread(image_path)
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     print("Applying threshold...")
     threshold = apply_threshold(gray_img)
-    print("Detecting edges and drawing contours...")
-    edges_detected = detect_edges_and_draw_contours(threshold, gray_img)
 
-    kernel = np.ones((3,3),np.uint8)
+    print("Discovering connected components...")
+    components_detected = detect_connected_components(threshold)
+
     print("Applying morphological transformations...")
-    opening_image = cv2.morphologyEx(edges_detected, cv2.MORPH_OPEN, kernel)
+    kernel = np.ones((3, 3), np.uint8)
+    opening_image = cv2.morphologyEx(components_detected, cv2.MORPH_OPEN, kernel)
     save_image(opening_image, 'images/opening.png')
+
     print("Applying watershed algorithm...")
     markers = apply_watershed(opening_image, gray_img, image)
+
     num_stars = count_stars(markers)
     print(f"Number of stars found: {num_stars}")
 
